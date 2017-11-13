45038f21814207760c5a196d3f8848b0b556ba8b
Nicanor <nicanor5+github@gmail.com>
2017-11-13 14:24:55 +0100

    Minor prompt change

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
+git submodule init
+git submodule update
 echo "Enter the full path to the Kaldi installation root: "
 read kaldi_root
 shellname=$(basename $SHELL)
 echo "export KALDI_ROOT=\"${kaldi_root}\"" >> "$HOME/.${shellname}rc"
-echo "You will need to restart your shell for the changes to take effect"
+echo "You will need to restart your shell for the changes to take effect."
